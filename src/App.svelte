7592b1adafe851527c12f75d84b885d92b0ccc6c
VexdStudios <36055611+Zaxiify@users.noreply.github.com>
2022-09-11 20:36:33 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { Button, Heading, Mark, Span, P, Label, Iconinput } from 'flowbite-svelte';
+  import { slide, fade, fly } from 'svelte/transition';
+  import { onMount } from 'svelte';
+  import { cubicInOut, sineInOut } from 'svelte/easing';
+
+  let ready = false;
+  onMount(() => ready = true);
+</script>
+
+{#if ready}
+<div in:slide="{{ easing: cubicInOut, duration: 1500 }}" out:slide>
+  <h1 class="gamer">
+    bbot-r
+  </h1>
+  <p>
+    bbot, reimagined.
+  </p>
+  <p class="seperator">‎ </p>
+  <hr />
+  <p class="seperator">‎ </p>
+  <button>script</button>
+</div>
+{/if}
---
+++ src/lib/Buttons.svelte
@@ -0,0 +1,5 @@
+<script>
+    import { Button } from 'flowbite-svelte';
+</script>
+
+<Button outline gradient color="purpleToBlue" class="outline">Purple to Blue</Button>
